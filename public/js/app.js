4a3cae65f1f6c0089877ec5e9d4764aa1605ae76
Ian Candy <iancandysemail@gmail.com>
2015-08-19 19:08:40 -0400

    add comments

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,6 @@
 $(function(){
 	$("#toggler").click(function(){
-		$("p#lorem").slideToggle();
+		$("p#lorem").slideToggle(); //using slide toggle because I think it looks nice, but could easily just hide, fade, etc. 
 		if($(this).text() == 'Hide Text')
     {
         $(this).text('Show Text');
